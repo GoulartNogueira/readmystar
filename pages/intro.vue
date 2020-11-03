b7afe5d5fcf5c627431107bc3628301cfdd6a44a
Andre Goulart <goulartnogueira@gmail.com>
2020-11-03 14:08:35 -0300

    Text generated by GPT2

---
+++ pages/intro.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>Introduction</h3>
+    <p>We teach you how to read your stars so you can write your own story.</p>
+    <p>
+      In a world of seven billion people, what makes your life any different
+      from anyone else's? What are the characteristics that define you? Your
+      personality, your traits, and your experiences all shape who you are and
+      what you will become.
+    </p>
+    <p>
+      To an extent, the planets and stars already know all of this. They know
+      your past and predict your future. Your zodiac sign is a reflection of who
+      you are and what is to come.
+    </p>
+    <p>
+      Read My Star offers free natal charts and interpretations for all signs.
+      We aim to provide insight into who you are and what makes you tick. We
+      will explain your past, present, and future. We will point you in the
+      right direction and help you become the best version of yourself.
+    </p>
+    <p>What are you going to do about it? The answers are in your stars.</p>
+    <p>These are your stars. This is your life. This is your story.</p>
+  </div>
+</template>
+
+<script></script>
