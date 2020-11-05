22fe0b158eceda183b3634de6756f60707b54e85
Andre Goulart <goulartnogueira@gmail.com>
2020-11-05 14:35:18 -0300

    particles in layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -42,7 +42,6 @@
       </v-btn>
     </v-app-bar> -->
     <v-main>
-      <ParticleBackground />
       <v-container>
         <nuxt />
       </v-container>
@@ -64,12 +63,7 @@
 </template>
 
 <script>
-import ParticleBackground from '~/components/ParticlesBackground.vue'
-export default {
-  components: {
-    ParticleBackground,
-  },
-  /* data() {
+/* data() {
     return {
       clipped: false,
       drawer: false,
@@ -92,5 +86,24 @@ export default {
       title: 'Vuetify.js',
     }
   }, */
-}
 </script>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
